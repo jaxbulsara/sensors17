0b54bb9682bcf10c937ddb4f1ffa5a0406a96f1a
Jay Bulsara <jaxbulsara@gmail.com>
2017-11-21 00:00:15 -0500

    completed advanced simon says

--- Project Tutorials/Simon Says/advanced_simon_says/advanced_simon_says.ino
+++ Project Tutorials/Simon Says/advanced_simon_says/advanced_simon_says.ino
@@ -26,7 +26,7 @@ const byte btn_clock = 9;
 const byte btn_latch = 10;
 
 // setup buzzer pin
-const byte speakerPin = 11;
+const byte speaker_pin = 11;
 
 // COMPONENTS
 // number of options in the game
@@ -41,6 +41,9 @@ unsigned int led_register = 0;
 // stores current button state
 byte btn_state [num_of_options];
 
+// variable to hold which button is pressed
+byte which_button;
+
 // setup tone frequency to be played by buzzer
 const unsigned int notes [] = {523, 784, 830, 1397};
 
@@ -50,17 +53,14 @@ int i;
 // lcd object
 LiquidCrystal595 lcd(lcd_data, lcd_latch, lcd_clock);
 
-// ROUND VARIABLES
-// constant to hold the max number of rounds that can be played
-const byte maxRounds = 100;
+// stores the current highscore
+unsigned int highscore = 0;
 
-// variable to count the value of the current round
-byte roundCounter = 0;
-
-// setup array to hold the series of prompts for every round
-byte promptArray [maxRounds];
+// timing variable
+unsigned long startTime;
 
 void setup() {
+	// open serial channel for debugging
 	Serial.begin(9600);
 
 	// setup shift register pins as outputs
@@ -74,14 +74,11 @@ void setup() {
 
 	// clear leds
 	clearLed();
+	writeRegister();
 
 	// set up lcd
 	lcd.begin(16,2);
 	lcd.clear();
-
-	// setup up random seed with floating analog input
-	int seed = analogRead(0);
-	randomSeed(seed);
 }
 
 void loop() {
@@ -89,20 +86,29 @@ void loop() {
 	// wait for button press to play
 	lcd.clear();
 	lcd.setCursor(0,0);
-	lcd.print("Press any button");
+	lcd.print("Press a button.");
 	lcd.setCursor(0,1);
-	lcd.print("to play!");
+	lcd.print("Highscore: ");
+	lcd.print(highscore);
 
 	// wait for button and light the led of the button that was pressed.
 	// then wait for the button to be depressed
 	waitForBtnPress();
 	setLedByBtn();
 	writeRegister();
-	waitForDePress();
+	waitForDePress(0);
 	clearLed();
 	writeRegister();
 
+	// display ready to play message
+	lcd.clear();
+	lcd.setCursor(0,0);
+	lcd.print("Starting game!");
+
+	delay(2000);
+
 	// begin game
+	playGame();
 }
 
 // set the desired led HIGH or LOW
@@ -191,13 +197,22 @@ void waitForBtnPress() {
 				break;
 			}
 		}
+
+		delay(20);
 	}
 }
 
-void waitForDePress() {
+void waitForDePress(unsigned int timeout) {
 	bool pressFlag = 1;
+	unsigned long startTime = millis();
 
 	while (pressFlag) {
+		if (timeout) {
+			if (millis() - startTime >= 200) {
+				break;
+			}
+		}
+
 		btn_read();
 		for (i = 0; i < num_of_options; i++) {
 			// if every button is depressed, then pressFlag will stay false and end the while loop
@@ -209,6 +224,155 @@ void waitForDePress() {
 				break;
 			}
 		}
-		Serial.println("");
+
+		delay(20);
 	}
+}
+
+byte checkButton() {
+	// assumes the user only presses one button
+	// the first pressed button from the left will be passed on
+	for (i = 0; i < num_of_options; i++) {
+		if (btn_state[i] == 1) {
+			return i;
+		}
+	}
+
+	// if no button is pressed, return number of options
+	return num_of_options;
+}
+
+void playGame() {
+	// setup up random seed with floating analog input
+	int seed = analogRead(0);
+	randomSeed(seed);
+
+	// ROUND VARIABLES
+	// constant to hold the max number of rounds that can be played
+	const byte maxRounds = 100;
+
+	// variable to count the value of the current round
+	byte round_counter = 0;
+
+	// user turn round counter
+	byte user_round;
+
+	// setup array to hold the series of prompts for every round
+	byte prompt_array [maxRounds];
+
+	// true if the user hasn't messed up
+	bool play_flag = 1;
+
+	while (play_flag) {
+		// display round number and high score
+		lcd.clear();
+		lcd.setCursor(0,0);
+		lcd.print("Round ");
+		lcd.print(round_counter + 1);
+		lcd.setCursor(0,1);
+		lcd.print("High score: ");
+		lcd.print(highscore);
+
+		// wait a bit before displaying prompt
+		delay(2000);
+
+		// generate a random number between 0 and 3 and put it in the next available spot in the prompt array
+		prompt_array[round_counter] = random(4);
+
+		// blink each led in the sequence of prompts
+		for (byte i = 0; i <= round_counter; i++) {
+			// turn on the led specified by the current index of prompt_array
+			setLed(prompt_array[i], HIGH);
+			writeRegister();
+
+			// stay on for a bit
+			delay(200);
+
+			// clear leds
+			clearLed();
+			writeRegister();
+
+			delay(200);
+		}
+
+		// round counting variable initialization
+		user_round = 0;
+
+		// player turn
+		while (1) {
+			// // wait for button press
+			waitForBtnPress();
+
+			// check which button is pressed
+			which_button = checkButton();
+
+			Serial.print("Button ");
+			Serial.println(which_button);
+
+			// check if the pressed button is correct
+			if (which_button != num_of_options) {
+				if (which_button == prompt_array[user_round]) {
+					// light led for 200 ms or until button depress
+					// whichever comes first
+					setLed(prompt_array[user_round], HIGH);
+					writeRegister();
+
+					// wait for button depress with timeout of 200 ms for led to turn off
+					waitForDePress(200);
+
+					clearLed();
+					writeRegister();
+
+					// wait for true button depress if still pressed
+					waitForDePress(0);
+				}
+
+				// if wrong, end game
+				else {
+					// turn all leds on for visual cue
+					for (i = 0; i < num_of_options; i++) {
+						setLed(i, HIGH);
+					}
+					writeRegister();
+
+					// display game over
+					lcd.clear();
+					lcd.setCursor(0,0);
+					lcd.print("GAME OVER.");
+
+					delay(2000);
+
+					clearLed();
+					writeRegister();
+
+					delay(3000);
+
+					// set play flag false so game loop ends
+					play_flag = 0;
+					break;
+				}
+			}
+
+            //
+			// if the play counter is equal to the round counter, the sequence has been completed.
+			// exit the user round loop
+			if (user_round == round_counter) {
+				delay(1000);
+				break;
+			}
+
+			user_round++;
+		}
+
+		// increment round counter
+		round_counter++;
+
+		// set new highscore if new record is set
+		// round counter is one less than actual round, so this is done after the increment
+		if (round_counter > highscore) {
+			highscore = round_counter;
+		}
+	}
+
+
 }
